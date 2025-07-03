2ba15c3a2a193fd24cb6a9db2ca094acaefb4d66
Franz Geffke <m@f-a.nz>
2025-07-03 19:29:00 +0100

    fixed encoding issue uploading to aws

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ PROFILE_NAME="f-a.nz"
 CLOUDFRONT_ID="E39BB7KBN2R6LQ"
 
 # Gulp
-guix shell node pnpm imagemagick graphicsmagick -- pnpm dlx gulp || exit 1
+guix shell node pnpm imagemagick graphicsmagick -- pnpm run build:assets || exit 1
 
 # Site
 guix shell ruby@3.1 make gcc-toolchain -- sh -c "export BUNDLE_PATH=.bundle && bundle install && bundle exec jekyll build -d $FOLDER_NAME" || exit 1
@@ -17,7 +17,8 @@ echo "Uploading folder: $FOLDER_NAME"
 echo "Destination: $AWS_BUCKET_URL"
 
 # Upload the folder to S3 using AWS CLI
-guix shell awscli -- aws s3 sync $FOLDER_NAME $AWS_BUCKET_URL --profile $PROFILE_NAME
+guix shell awscli -- aws s3 sync $FOLDER_NAME $AWS_BUCKET_URL --profile $PROFILE_NAME --content-type "text/html; charset=utf-8" --exclude "*" --include "*.html"
+guix shell awscli -- aws s3 sync $FOLDER_NAME $AWS_BUCKET_URL --profile $PROFILE_NAME --delete
 
 # Invalidate CloudFront (Uncomment if you need this)
 guix shell awscli -- aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_ID --paths "/*" --profile $PROFILE_NAME
