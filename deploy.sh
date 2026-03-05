61bb4e5cd327eb91fd52a3a7eaeaf8c00a0b7eb9
Franz Geffke <mail@gofranz.com>
2026-03-05 21:45:18 +0000

    enable wkd

--- deploy.sh
+++ deploy.sh
@@ -18,6 +18,7 @@ echo "Destination: $AWS_BUCKET_URL"
 
 # Upload the folder to S3 using AWS CLI
 guix shell awscli -- aws s3 sync $FOLDER_NAME $AWS_BUCKET_URL --profile $PROFILE_NAME --content-type "text/html; charset=utf-8" --exclude "*" --include "*.html"
+guix shell awscli -- aws s3 cp $FOLDER_NAME/.well-known/openpgpkey/hu/ $AWS_BUCKET_URL/.well-known/openpgpkey/hu/ --recursive --content-type "application/octet-stream" --profile $PROFILE_NAME
 guix shell awscli -- aws s3 sync $FOLDER_NAME $AWS_BUCKET_URL --profile $PROFILE_NAME --delete
 
 # Invalidate CloudFront (Uncomment if you need this)
